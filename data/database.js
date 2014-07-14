09432c854834da38fb366d1dd47fd405efab1f0d
Elana Stroud <elanas@wix.com>
2014-07-14 16:00:16 +0300

    set up database getSetting operation

--- data/database.js
+++ data/database.js
@@ -4,27 +4,44 @@
  *
  *
  */
+
 // require database files
 var mongojs = require("mongojs");
-var db = mongojs("db",["rollingnotes"]).rollingnotes;
+var db = mongojs("db",["rollingnotes"]);
+// require default JSON objects for new instances   
+var defaults = require("./defaults");
 
+//inserts new widget instance or loads existing one
+function getSettings(uniqueid, callback) {
+    db.rollingnotes.findOne({_id: uniqueid}, function(err, doc) {
+        var settings;
+        if(err || !doc) {
+            console.log('Settings Doc did not exist, was created and returned');
+            settings = defaults.settings;
+            settings._id = uniqueid;
+            db.rollingnotes.insert(settings);
+        } else {
+            console.log('Settings Doc existed and returned');
+            settings = doc;
+        }
+        callback(settings);
+    });
+};
 
-//inserts a new widget instance
-function insertComponent(id) {
-    db.insert()
+// TODO: write function that sends settings to UI/css
+function printSettings(settings) {
+    console.log("PRINT SETTINGS: " + JSON.stringify(settings));
+};
+
+// TODO: replicate for widget endpoint
+
+// for testing purposes
+function loadDB() {
+ //  db.rollingnotes.remove();
+//    db.rollingnotes.insert({'test': '2'});
+//    db.rollingnotes.insert({'test': '3'});
+//    db.rollingnotes.insert({'test': '4'});
 }
 
-
-//inserts a new note in existing widget
-
-
-
-
-//db.rollingnotes.find({'test':'success'}, function(err, user){
-//    console.log(user[3]);
-//})
-
-
-//console.log(db.rollingnotes.find(function (err, docs){
-//    console.log(docs);
-//}));
+loadDB();
+getSettings("6", printSettings);
---
+++ data/defaults.js
@@ -0,0 +1,7 @@
+/**
+ * Created by elanas on 7/14/14.
+ */
+
+exports.settings = {
+    "test" : "0"
+};
