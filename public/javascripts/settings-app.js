b74689fe611ab40f72b19aa8011344f74bcd8da6
adamcolewix <adamc@wix.com>
2014-08-03 19:36:45 +0300

    slider now updated properly when notes are hidden/shown, note no longer automatically rolls on refresh

--- public/javascripts/settings-app.js
+++ public/javascripts/settings-app.js
@@ -53,6 +53,14 @@ var templates = require("./defaultTemplates");
 //          updateComponent(settings);
         };
 
+        this.updateSlider = function() {
+//          Wix.Settings.refreshAppByCompIds([parseCompId(settings._id)]);
+            settings.transition.updateSlider = true;
+            updateComponent(settings);
+            settings.transition.updateSlider = false;
+//          updateComponent(settings);
+        };
+
 
         Wix.UI.onChange('template', function(newSettings){
             settings.design.template = newSettings.value;
@@ -203,7 +211,7 @@ var templates = require("./defaultTemplates");
                 el.closest('.content-row').find('.note-text').removeClass('unwatched-note');
                 settings.notes[index].visibility = true;
             }
-            this.previewTransition();
+            this.updateSlider();
             updateComponent(settings);
 
         }
