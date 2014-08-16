55aca5a4586c9891067d431464e4ba54025ad3c4
Elana Stroud <elanas@wix.com>
2014-08-17 02:13:21 +0300

    changed to promised-mongo db

--- data/database.js
+++ data/database.js
@@ -2,24 +2,31 @@
  * Rolling Notes database using mongoDB to save note settings
  **************************/
 
-var mongojs = require('mongojs');
+var pmongo = require('promised-mongo');
+
+// require database files
+//var mongojs = require('mongojs');
 var mongoUri = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://localhost/db';
 
 var collections = ["rollingnotes"]
-var db = mongojs.connect(mongoUri, collections);
+//var db = mongojs.connect(mongoUri, collections);
+var db = pmongo.connect(mongoUri, collections);
 
 
-// require database files
-//var mongojs = require("mongojs");
-//var db = mongojs("db",["rollingnotes"]);
-// require default JSON objects for new instances   
+
+// require default JSON objects for new instances
 var templates = require("../public/javascripts/defaultTemplates");
+
+//TODO add input validation on key and callback
+//TODO PROMISES, not callbacks
 
 //inserts new widget instance or loads existing one
 function getCompByKey(key, callback) {
     // search for instance of setting in databse by unique key
+    //TODO what should happen on errors? why insert something?
+    //TODO what happens if doc doesn't exist?
     db.rollingnotes.findOne({_id: key}, function(err, doc) {
         var comp;
         if(err || !doc) {
@@ -40,6 +47,8 @@ function getCompByKey(key, callback) {
     });
 };
 
+
+//TODO change this to use promises
 function updateComponent(updatedComp) {
     db.rollingnotes.save(updatedComp, function(err, data) {
         if (err)  {
