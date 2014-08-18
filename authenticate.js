47483551429e3e96666807b935921a4bcd2c15fd
adamcolewix <adamc@wix.com>
2014-08-18 12:48:06 +0300

    merging with Elana

--- authenticate.js
+++ authenticate.js
@@ -7,8 +7,12 @@ var express = require('express');
 var app = express();
 var wix = require('openapi-node');
 
-var APP_SECRET = '274cac4b-5816-46a5-b9e6-c9c04c52c46e';
-var APP_ID = '137f851f-ed9d-9e85-4e0a-b73bc11455e4';
+//heroku
+//var APP_SECRET = '274cac4b-5816-46a5-b9e6-c9c04c52c46e';
+//var APP_ID = '137f851f-ed9d-9e85-4e0a-b73bc11455e4';
+//local
+var APP_SECRET = '8ede2ed1-9961-4b3c-9bf9-a154eee00ddb';
+var APP_ID = '138a0642-8449-ff5d-546a-445737e36967';
 
 exports.authenticate = function authenticate(req, res) {
     var instance = req.query.instance;
