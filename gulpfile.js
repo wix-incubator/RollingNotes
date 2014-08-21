322bcd71adda7848143abe43df0bef2316ebf46e
adamcolewix <adamc@wix.com>
2014-08-21 06:38:19 +0300

    working on dat gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 
-
 // plugins
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
@@ -10,22 +9,9 @@ var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 
 var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-    // place code for your default task here
-});
 
-gulp.task('browserify', function() {
-    gulp.src(['./public/javascripts/settings-app.js'])
-        .pipe(browserify({
-            insertGlobals: true,
-            debug: true
-        }))
-        .pipe(concat('bundled.js'))
-        .pipe(gulp.dest('./app/js'))
-});
-
+var mainBowerFiles = require('main-bower-files');
 
 // tasks
 gulp.task('lint', function() {
@@ -39,12 +25,14 @@ gulp.task('clean', function() {
     gulp.src('./dist/*')
         .pipe(clean({force: true}));
 });
+
 gulp.task('minify-css', function() {
     var opts = {comments:true,spare:true};
     gulp.src(['./public/stylesheets/*.css', '!./app/bower_components/**'])
         .pipe(minifyCSS(opts))
         .pipe(gulp.dest('./dist/'))
 });
+
 gulp.task('minify-js', function() {
     gulp.src(['./public/javascripts/settings-app.js', '!./public/javascripts/bower_components/**'])
         .pipe(uglify({
@@ -53,21 +41,45 @@ gulp.task('minify-js', function() {
         }))
         .pipe(gulp.dest('./dist/'))
 });
-gulp.task('copy-bower-components', function () {
-    gulp.src('./public/javascripts/bower_components/**')
-        .pipe(gulp.dest('dist/bower_components'));
-});
+
+
 gulp.task('copy-html-files', function () {
     gulp.src('./views/*.ejs')
         .pipe(gulp.dest('dist/'));
 });
+//
+//<script type="text/javascript" src="javascripts/bower_components/jqueryui/jquery-ui.min.js"></script>
+//    <script type="text/javascript" src="//sslstatic.wix.com/services/js-sdk/1.33.0/js/Wix.js"></script>
+//    <script type="text/javascript" src="javascripts/bower_components/angular/angular.min.js"></script>
+//    <script type="text/javascript" src="javascripts/bower_components/angular-ui-sortable/sortable.min.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/wix-ui-lib2/ui-lib.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/angular-mailto/angular-mailto.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/visibilityjs/lib/visibility.core.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/visibilityjs/lib/visibility.timers.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/deep-diff/releases/deep-diff-0.2.0.min.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/angular-animate/angular-animate.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/lodash/dist/lodash.js"></script>
+//    <script type="text/javascript" src="/javascripts/bower_components/slimScroll/jquery.slimscroll.js"></script>
+//    <script type="text/javascript" src="/javascripts/settings-bundle.js"></script>
+gulp.task('bowerFiles', function() {
+      gulp.src([
+         "public/javascripts/bower_components/jquery/dist/jquery.min.js",
+         "public/javascripts/bower_components/jqueryui/jquery-ui.min.js",
+         "public/javascripts/bower_components/angular/angular.min.js"
 
+
+     ])
+         .pipe(concat("bower-bundle.js"))
+         .pipe(uglify())
+         .pipe(gulp.dest('dist/'));
+});
 
 // default task
 gulp.task('default',
     ['lint']
 );
+
 // build task
 gulp.task('build',
-    ['lint', 'minify-css', 'minify-js', 'copy-html-files', 'copy-bower-components']
+    ['lint', 'minify-css', 'minify-js',  'copy-html-files']
 );
