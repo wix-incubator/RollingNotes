454390c3c013b14334b912588ec7c9f6db449b25
adamcolewix <adamc@wix.com>
2014-08-25 11:48:30 +0300

    fixed package for heroku

--- authenticate.js
+++ authenticate.js
@@ -8,15 +8,14 @@
 var express = require('express');
 var app = express();
 var wix = require('openapi-node');
-
 var config = require('./config');
 
 //heroku
-var APP_SECRET = '274cac4b-5816-46a5-b9e6-c9c04c52c46e';
-var APP_ID = '137f851f-ed9d-9e85-4e0a-b73bc11455e4';
+//var APP_SECRET = '274cac4b-5816-46a5-b9e6-c9c04c52c46e';
+//var APP_ID = '137f851f-ed9d-9e85-4e0a-b73bc11455e4';
 //local
-//var APP_SECRET = config.APP_SECRET;
-//var APP_ID = config.APP_ID;
+var APP_SECRET = config.APP_SECRET;
+var APP_ID = config.APP_ID;
 
 /**
  * Exports the authenticate function to be used as middleware
